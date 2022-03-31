3756400f45d1df65249ae05d48efd458534363eb
Patrick Muldoon <100616270+muldoonpatrick@users.noreply.github.com>
2022-03-31 09:54:24 -0500

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,5 +1,3 @@
-FROM debian:9.5-slim
+#!/bin/sh -l
 
-ADD entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
+sh -c "echo Hello world my name is $INPUT_MY_NAME"
